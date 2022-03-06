d7deabd38a033d39840da4749484e58e27fcd94e
wonjongbot0 <wonjongbot@gmail.com>
2022-03-06 09:19:16 +0000

    pretty sure stuff is working now

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ app = Flask(  # Create a flask app
 	__name__,
 	template_folder='templates',  # Name of html file folder
 	static_folder='static'  # Name of directory for static files
+  
 )
-
+locations = {}
 
 @app.route('/')  # What happens when the user visits the site
 def base_page():
@@ -20,10 +21,24 @@ def base_page():
 	)
 
 
-def add_location(locations, bathroom_name, r_clean, r_smell, r_acess, r_over):
+def add_location(locations, bathroom_name, r_clean, r_smell, r_acess):
   # extends locations list
-  locations.extend(bathroom_name, r_clean, r_smell, r_acess, r_over)
+  if not(bathroom_name in locations) :
+    
+    locations[bathroom_name] = [r_clean,   r_smell, r_acess, 0]
 
+def edit_location(locations, bathroom_name, r_clean, r_smell, r_acess) :
+  if bathroom_name in locations :
+    count = locations[bathroom_name][3] + 1
+    locations[bathroom_name][3] = count
+    clean = (r_clean +   locations[bathroom_name][0])/count
+    smell = (r_smell +   locations[bathroom_name][1])/count
+    acess = (r_acess +   locations[bathroom_name][2])/count
+    locations[bathroom_name] = [clean,   smell, acess, count]
+  else :
+    #return to review bathroom site and add a boolean variable syaing that the bathroom does not exist then edit the code on site_2 with a script tag saying that if the variable equals true print like the bathroom does not exist in databse
+    dne = true
+  
 
 @app.route('/2', methods = ['GET', 'POST'])
 def page_report():
@@ -35,23 +50,40 @@ def page_report():
     r_acess = request.form['facess']
     r_over = request.form['fover']
     print(bathroom_name)
-    add_location(locations, bathroom_name, r_clean, r_smell, r_acess, r_over)
+    edit_location(locations, bathroom_name, r_clean, r_smell, r_acess)
   return render_template('site_2.html')
   
-@app.route('/3')
+@app.route('/3', methods=['GET', 'POST'])
 def page_find() :
-  if request.method == "POST":
-    positiondata = request.get_json()
-    print(positiondata)
   return render_template('site_3.html')
 
-@app.route('/4')
+
+@app.route('/position_data', methods=['GET', 'POST'])
+def get_position() :
+  if request.method == "POST":
+    position_data = request.get_json()
+    print(position_data)
+  results = {'processed': 'true'}
+  return jsonify(results)
+
+@app.route('/4', methods = ['GET','POST'])
 def page_new() :
+  if request.method == 'POST':
+    bathroom_name = request.form["fname"]
+    r_clean = request.form['fclean']
+    r_smell = request.form['fsmell']
+    r_acess = request.form['facess']
+    add_location(locations, bathroom_name, r_clean, r_smell, r_acess)
+    print(locations[bathroom_name])
   return render_template('site_4.html')
 
-
+@app.route('/debugging')
+def page_debug() :
+  return render_template('site_5.html', locations = locations)
+  
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
+    
 		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
 		port=rand.randint(2000, 9000)  # Randomly select the port the machine hosts on.  
 	)
